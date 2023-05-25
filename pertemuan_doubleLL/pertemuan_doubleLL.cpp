3fd38019df1eceec9d62f3c54d6550584a6fdfb0
giant-paw <giantpawbusiness@gmail.com>
2023-05-25 15:30:32 +0700

    pembuatan beberapa fungsidi dalam class DoubleLL

--- pertemuan_doubleLL/pertemuan_doubleLL.cpp
+++ pertemuan_doubleLL/pertemuan_doubleLL.cpp
@@ -10,6 +10,21 @@ public:
 	Node* prev;
 };
 
+class DoubleLinkedList {
+private:
+	Node* START;
+public:
+	DoubleLinkedList();
+	void addNote();
+	bool search(int rollNo, Node** previous, Node** current);
+	bool deleteNode(int rollNo);
+	bool listEmpty();
+	void traverse();
+	void retraverse();
+	void hapus();
+	void searchData();
+};
+
 int main()
 {
 }
