29d4647017826150e10c0b6ad922ba978e00b0ed
giant-paw <giantpawbusiness@gmail.com>
2023-05-25 15:36:05 +0700

    pembuatan node baru bernama 'newNode'

--- pertemuan_doubleLL/pertemuan_doubleLL.cpp
+++ pertemuan_doubleLL/pertemuan_doubleLL.cpp
@@ -36,6 +36,9 @@ void DoubleLinkedList::addNote() {
 	cin >> nim;
 	cout << "\nEnter the name of the Student : ";
 	cin >> nm;
+	Node* newNode = new Node();			//step 1
+	newNode->noMhs = nim;				//step 2
+	newNode->name= nm;					//step 2
 }
 
 int main()
