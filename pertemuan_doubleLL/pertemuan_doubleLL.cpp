ed84c947b59674283dc083caf6a8a8d68adbad50
giant-paw <giantpawbusiness@gmail.com>
2023-05-25 15:42:11 +0700

    logika pengecekan start dan penempatan new nnode sebelum masuk ke node

--- pertemuan_doubleLL/pertemuan_doubleLL.cpp
+++ pertemuan_doubleLL/pertemuan_doubleLL.cpp
@@ -39,6 +39,17 @@ void DoubleLinkedList::addNote() {
 	Node* newNode = new Node();			//step 1
 	newNode->noMhs = nim;				//step 2
 	newNode->name= nm;					//step 2
+
+	//insert a node in the beginning of a doubly - linked list
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs) {
+			cout << "\nDuplicate number not allowed" << endl;
+			return;
+		}
+		newNode->next = START;			//step 3
+		if (START != NULL)
+			START->prev = newNode;		//step4
+	}
 }
 
 int main()
