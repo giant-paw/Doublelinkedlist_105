8d932c71add5adceabc7be2417913968f7249324
giant-paw <giantpawbusiness@gmail.com>
2023-05-25 16:02:44 +0700

    penempatan current dan previous guna penempatan newNode

--- pertemuan_doubleLL/pertemuan_doubleLL.cpp
+++ pertemuan_doubleLL/pertemuan_doubleLL.cpp
@@ -66,6 +66,12 @@ void DoubleLinkedList::addNote() {
 		cout << "\nDuplicate roll numbers not allowed" << endl;
 		return;
 	}
+
+	newNode->next = current->next;		//step 4
+	newNode->prev = current;			//step 5
+	if (current->next != NULL)
+		current->next->prev = newNode;	//step 6
+	current->next = newNode;			//step 7
 }
 
 int main()
